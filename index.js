de4446ad37322031cae75122e8c4ac83884b6d4e
Lewis Jones Ndubi <lamarjonesndubi@gmail.com>
2024-07-04 08:14:58 +0300

    adds all

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+// Alright..lets do this...
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Example usages:
+  console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+  countDown(10);
+  
