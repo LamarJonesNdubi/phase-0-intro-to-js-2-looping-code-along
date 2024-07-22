d54ab6be779d669c3417bf6e6fd51e40bec9afd7
Lewis Jones Ndubi <lamarjonesndubi@gmail.com>
2024-07-22 20:26:59 +0300

    adds all

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 // Code your solutions in this file
 // Alright..lets do this...
-function writeCards(names, event) {
-    let messages = [];
-    for (let i = 0; i < names.length; i++) {
-      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+
+// Code your solutions in this file
+
+gifts=[]
+
+function wrapGifts(gifts) {
+  let i = 0; // the initialization moves OUTSIDE the body of the loop!
+  while (i < gifts.length) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    i++; // the iteration moves INSIDE the body of the loop!
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
     }
+  }
+countDown(10)
+
+function writeCards(names, occasion) {
+    let messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+  
     return messages;
   }
-  
-  function countDown(number) {
-    while (number >= 0) {
-      console.log(number);
-      number--;
-    }
-  }
-  
-  // Example usages:
-  console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
-  countDown(10);
-  
